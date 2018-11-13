94909c37a03318aa0a80e56e2d6408e3cb28832f
Steven Olufemi-OluKa <Steven.Olufemi-OluKanni@jet2.com>
2018-11-13 17:22:57 +0000

    work done at work. must sort out dependecy injection issue

--- ConsumeApi/Controllers/HomeController.cs
+++ ConsumeApi/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsumeApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,17 @@ namespace ConsumeApi.Controllers
 {
     public class HomeController : Controller
     {
+        private PostcodesApi _postcodesApi;
+
+        public HomeController(PostcodesApi postcodesApi)
+        {
+            _postcodesApi = postcodesApi;
+        }
         public ActionResult Index()
-        {
+        {        
+            ViewData["constituency"] = _postcodesApi.GetParliamentaryConstituency("ne46ap");
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ ConsumeApi/Models/ConsumeApi.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace ConsumeApi.Models
+{
+    public class PostcodesApi
+    {
+        private string baseUrl = "https://postcodes.io/";
+        
+        public string GetParliamentaryConstituency(string postcode)
+        {
+            string extention = $"/postcodes/{postcode}";
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(baseUrl);
+            var response = client.GetAsync(extention).Result;
+            string result = response.Content.ReadAsStringAsync().Result;
+            var jobject = JsonConvert.DeserializeObject<JObject>(result);
+            return jobject["result"]["parliamentary_constituency"].ToString();
+
+        }
+
+    }
+}
